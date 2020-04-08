da2d670ad118116e4cd159f35623b0f42196f6eb
Diego F. Reyes @diegofreyesweb <diferrev@gmail.com>
2020-04-08 10:07:44 -0500

    Localización de librerías en recursos externos, arrays de cache de cada pantalla en SW

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,57 @@
 const CACHE_NAME = 'app-cache-v8';
-const CACHED_URLS = [
+const CACHED_URLS_BASIC = [
   'favicon.ico',
-  'index.html',
-  'catalog.html',
   'assets/img/sample-logo.png',
   'assets/fonts/fa-solid-900.woff2',
   'assets/fonts/Rubik-Regular.woff',
+  'assets/fonts/Rubik-Bold.woff',
   'assets/css/animate.css',
   'assets/css/style.css',
   'assets/js/scripts.js',
   'simulator/js/jquery.js',
-  'assets/media/sample.mp4',
-  'assets/products.json',
 ];
+
+const CACHED_URLS_INDEX = [
+  'index.html',
+  'assets/media/sample.mp4',
+]
+
+const CACHED_URLS_MAP = [
+  'map.html', //Activos del mapa
+  'assets/img/map.png',
+  'assets/img/qr-example.png',
+]
+
+const CACHED_URLS_CATALOG = [
+  'catalog.html',
+  'assets/products.json',
+  'assets/products/AM13423.jpg',
+  'assets/products/AR1000.jpg',
+  'assets/products/CA0541.jpg',
+  'assets/products/COM8028.jpg',
+  'assets/products/CS2330.jpg',
+  'assets/products/ES4103.jpg',
+  'assets/products/MC3000.jpg',
+  'assets/products/MN3710.jpg',
+  'assets/products/MTV17519.jpg',
+  'assets/products/SF9459.jpg',
+]
+
+const CACHED_URLS_SIMULATOR = [
+  'simulator/index.html',
+]
+
+const CACHED_URLS = [
+  ...CACHED_URLS_BASIC,
+  ...CACHED_URLS_INDEX,
+  ...CACHED_URLS_CATALOG,
+  ...CACHED_URLS_MAP,
+  ...CACHED_URLS_SIMULATOR
+]
+
+console.log( CACHED_URLS )
+
+
 
 self.addEventListener('install', event => {
   console.log('Service worker install event!');
@@ -42,9 +81,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('Fetch intercepted for:', event.request.url);
-  
   event.respondWith(fromCache( event.request ));
-
   event.waitUntil( update(event.request ));
 
 });
